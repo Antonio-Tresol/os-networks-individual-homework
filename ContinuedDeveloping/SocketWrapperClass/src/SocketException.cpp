540cf766144b9ee3578d5b025bde8714ac141d37
Antonio Badilla-Olivas <anthonny.badilla@ucr.ac.cr>
2023-04-28 09:14:12 -0600

    Adding constructors to SocketException. Modifying one to accept messages without error codes to use in throw with nested calls.

--- ContinuedDeveloping/SocketWrapperClass/src/SocketException.cpp
+++ ContinuedDeveloping/SocketWrapperClass/src/SocketException.cpp
@@ -1,23 +1,45 @@
 #include "SocketException.hpp"
 
 SocketException::SocketException(const std::string& message,
-    const std::string& function, int errorCode)
-    : m_message(message), m_function(function), m_errorCode(errorCode) {
-        this->m_message = errorMessage();
-    }
+  const std::string& function, int errorCode)
+  : mMessage(message), mFunction(function), mErrorCode(errorCode) {
+      this->mErrorMessage = errorMessage();
+  }
 
+SocketException::SocketException(const std::string& message, 
+  const std::string& function, bool isSslError)
+  : mMessage(message), mFunction(function) {
+  if (isSslError) {
+    this->mErrorMessage = sslError(mMessage, mFunction);
+  } else {
+    this->mErrorMessage = mMessage + " (" + mFunction + ")";
+  }
+}
 const char* SocketException::what() const noexcept {
-    return this->m_message.c_str();
+  return this->mErrorMessage.c_str();
 }
 
 const std::string& SocketException::function() const noexcept {
-    return m_function;
+  return mFunction;
 }
 
 int SocketException::errorCode() const noexcept {
-    return m_errorCode;
+  return mErrorCode;
 }
 
 std::string SocketException::errorMessage() const noexcept {
-    return m_message + " (" + m_function + ") - " + strerror(m_errorCode);
+  return mMessage + " (" + mFunction + ") - " + strerror(mErrorCode);
+}
+
+std::string SocketException::sslError(const std::string& message,
+  const std::string& function) const noexcept {
+  uint64_t error = ERR_get_error();
+  const char* buffer = ERR_reason_error_string(error);
+  std::string sslErrorMessage = message + " (" + function + ") - ";
+  if (buffer) {
+      sslErrorMessage += buffer;
+  } else {
+      sslErrorMessage += "NULL";
+  }
+  return sslErrorMessage;
 }
--- ContinuedDeveloping/SocketWrapperClass/src/SocketException.hpp
+++ ContinuedDeveloping/SocketWrapperClass/src/SocketException.hpp
@@ -10,6 +10,9 @@
 #include <string>
 #include <cstring>
 #include <cerrno>
+#include <unistd.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 
 /**
  * @class SocketException
@@ -28,13 +31,28 @@ class SocketException : public std::exception {
    */
   SocketException(const std::string& message, const std::string& function, 
     int errorCode);
-
+  /**
+   * @brief Constructor for SocketException for when the error code comes from
+   * the SSL library or when error code is not provided.
+   * @param message The error message to associate with the exception
+   * @param function is the function where error occurs
+   * @param bool isSSLError is a flag to find ssl (defualt), if false 
+   *  Socket Exception errorMessage will be just message and function.
+   */
+  SocketException(const std::string& message, const std::string& function,
+    bool isSslError = true);
   /**
    * @brief Returns a C-style character string describing the exception
    * @return A C-style character string describing the exception
    */
   virtual const char* what() const noexcept override;
 
+
+ private:
+  std::string mMessage; ///< The error message associated with the exception
+  std::string mFunction; ///< The name of the function that failed
+  int mErrorCode; ///< The error code associated with the failure
+  std::string mErrorMessage; ///< The error message to return
   /**
    * @brief Returns the name of the function that failed
    * @return The name of the function that failed
@@ -52,12 +70,11 @@ class SocketException : public std::exception {
    * @return A string describing the error
    */
   std::string errorMessage() const noexcept;
-
- private:
-  std::string m_message; ///< The error message associated with the exception
-  std::string m_function; ///< The name of the function that failed
-  int m_errorCode; ///< The error code associated with the failure
-  std::string m_errorMessage; ///< The error message to return
-
+  /**
+   * @brief gets the error message from the SSL library
+   * @param message the message to prepend to the error message
+   * @details depends on initialization of SSL libraries outside this class
+  */
+  std::string sslError(const std::string &message, const std::string& function) const noexcept;
 };
 #endif // SOCKET_EXCEPTION_HPP
